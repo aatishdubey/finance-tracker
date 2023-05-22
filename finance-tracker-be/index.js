6ffca91a98af052bd134c60fb3236f1769484756
Aatish Dubey <atsh_dubey@live.com>
2023-05-22 17:06:58 +0545

    updated cors origins

--- finance-tracker-be/index.js
+++ finance-tracker-be/index.js
@@ -16,7 +16,7 @@ app.use(
       "http://127.0.0.1:5174",
       "http://localhost:5174/",
       "http://localhost:5173/",
-      "https://finance-tracker-fe.onrender.com/",
+      "https://finance-tracker-fe.onrender.com",
     ],
   })
 );
